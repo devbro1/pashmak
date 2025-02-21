e2e80eecd89475cd97f2a9618e103f0f1da4b3bf
meow meow <meow@devbro.com>
2025-02-21 04:39:20 +0000

    test: adding hr db

--- src/databases/postgresql/PostgresqlConnection.ts
+++ src/databases/postgresql/PostgresqlConnection.ts
@@ -1,26 +1,23 @@
 import { Connection as ConnectionAbs } from '../../Connection';
-import { Connection, PoolClient } from 'pg';
+import { Connection, PoolClient, PoolConfig } from 'pg';
 import { Pool } from 'pg';
 import { CompiledSql } from '../../types';
 export class PostgresqlConnection extends ConnectionAbs {
     connection: PoolClient | undefined;
     static pool: Pool;
 
-    constructor() {
+    constructor(params: PoolConfig) {
         super();
         if(!PostgresqlConnection.pool) {
-        PostgresqlConnection.pool = new Pool({
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            port: 5432,
-            ssl: false,
-            max: 20, // set pool max size to 20
-            idleTimeoutMillis: 1000, // close idle clients after 1 second
-            connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-            maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
-          });
+            const defaults: PoolConfig = {
+                port: 5432,
+                ssl: false,
+                max: 20,
+                idleTimeoutMillis: 1000,
+                connectionTimeoutMillis: 1000,
+                maxUses: 7500,
+              };
+        PostgresqlConnection.pool = new Pool({...defaults, ...params});
         }
     }
     async connect(): Promise<boolean> {
--- tests/basic_queries.spec.ts
+++ tests/basic_queries.spec.ts
@@ -3,73 +3,90 @@ import { Query } from "../src/Query";
 import { Grammar } from "../src/Grammar";
 import { PostgresqlConnection } from "../src/databases/postgresql/PostgresqlConnection";
 import { Connection } from "../src/Connection";
-
+import { execSync } from "child_process";
 describe("raw queries", () => {
   let conn: Connection | null;
-  beforeEach(async () => {
-    conn = new PostgresqlConnection();
+
+  beforeAll(async () => {
+    const randName = Math.random().toString(36).substring(7);
+    const db_config = {
+      host: process.env.DB_HOST,
+      database: (process.env.DB_NAME || 'test_db') + `_${randName}`,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT || '5432'),
+    };
+
+    console.log("creating test database", db_config.database);
+    execSync(`psql --host ${db_config.host} --user ${db_config.user} --port ${db_config.port} postgres -c "CREATE DATABASE ${db_config.database}"`);
+    console.log("load database schema and data");
+    execSync(`psql --host ${db_config.host} --user ${db_config.user} --port ${db_config.port} -f ./tests/fixtures/load_hr_db_pg.sql ${db_config.database}`);
+
+    conn = new PostgresqlConnection(db_config);
     await conn.connect();
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await conn?.disconnect();
   })
 
   test("basic select all", () => {
-    let query = new Query( null, new Grammar());
-    query.table('table1');
+    const query = new Query( null, new Grammar());
+    query.table('countries');
     let r = query.toSql();
 
-    expect(r.sql).toBe("select * from table1");
+    expect(r.sql).toBe("select * from countries");
     expect(r.bindings.length).toBe(0);
 
-    query.whereOp('column1','=',1234);
+    query.whereOp('region_id','=',2);
 
     r = query.toSql();
 
-    expect(r.sql).toBe("select * from table1 where column1 = ?");
-    expect(r.bindings).toStrictEqual([1234]);
+    expect(r.sql).toBe("select * from countries where region_id = ?");
+    expect(r.bindings).toStrictEqual([2]);
 
 
-    query.whereOp('column2','=',222, 'or', true);
+    query.whereOp('country_id','=','BE', 'or', true);
 
     r = query.toSql();
 
-    expect(r.sql).toBe("select * from table1 where column1 = ? or not column2 = ?");
-    expect(r.bindings).toStrictEqual([1234,222]);
+    expect(r.sql).toBe("select * from countries where region_id = ? or not country_id = ?");
+    expect(r.bindings).toStrictEqual([2,'BE']);
   });
 
   test("basic connection functionality", async () => {
-    let query = new Query( conn, new Grammar());
-    query.table('users');
-    let r = query.toSql();
+    const query = new Query( conn, new Grammar());
+    query.table('countries');
+    const r = query.toSql();
 
-    expect(r.sql).toBe("select * from users");
+    expect(r.sql).toBe("select * from countries");
     expect(r.bindings.length).toBe(0);
 
     let result = await query.get();
-    expect(result.length).toBe(11);
+    expect(result.length).toBe(25);
 
-    query.whereOp('first_name','=','super');
-    query.whereOp('last_name','=','admin');
+    query.whereOp('country_id','=','CA');
+    query.whereOp('country_id','=','JP', 'or');
 
     result = await query.get();
-    expect(result.length).toBe(1);
-    expect(result[0].first_name).toBe('super');
+    expect(result.length).toBe(2);
+    expect(result[0].country_name).toBe('Canada');
   });
 
   test("basic connection functionality v2", async () => {
-    let query = new Query( conn, new Grammar());
-    query.table('clients');
-    let r = query.toSql();
+    const query = new Query( conn, new Grammar());
+    query.table('jobs');
+    const r = query.toSql();
 
-    expect(r.sql).toBe("select * from clients");
+    expect(r.sql).toBe("select * from jobs");
     expect(r.bindings.length).toBe(0);
 
-    let result = await query.get();
-    expect(result.length).toBe(1);
+    const result1 = await query.get();
+    expect(result1.length).toBe(19);
 
-    result = await query.get();
-    expect(result.length).toBe(1);
+    query.whereOp('job_title','ilike','P%');
+
+    const result2 = await query.get();
+    expect(result2.length).toBe(6);
   });
 });
