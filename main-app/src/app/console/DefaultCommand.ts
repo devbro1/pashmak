bfb750953cd56d772dfc8ca75d6a94689e60412b
Farzad Meow Khalafi <farzadk@gmail.com>
2025-05-27 15:10:39 -0400

    feat: working on migrate command branch name: 30-create-a-cli-lib

--- main-app/src/app/console/DefaultCommand.ts
+++ main-app/src/app/console/DefaultCommand.ts
@@ -2,6 +2,17 @@ import { Command, Option } from "clipanion";
 import { cli } from "@root/facades";
 
 export class DefaultCommand extends Command {
+  static usage = Command.Usage({
+    category: `Main`,
+    description: `server management command line.`,
+    details: `
+      The base command for running and managing your server.
+
+      Make sure you understand how things work.
+    `,
+    examples: [],
+  });
+
   async execute() {
     this.context.stdout.write(`Hello Default!\n`);
   }
--- main-app/src/app/console/MigrateCommand.ts
+++ main-app/src/app/console/MigrateCommand.ts
@@ -1,6 +1,14 @@
 import { cli } from "@root/facades";
 import { Command, Option } from "clipanion";
 
+/*
+pashmak make migration <FILENAME>
+pashmak migrate 
+pashmak migrate status # return status of existing migrations in db
+pashmak migrate rollback
+pashmak migrate refresh # doing all roll back then migrate
+pashmak migrate fresh # removing all tables then migrate
+*/
 export class MigrateCommand extends Command {
   static paths = [[`migrate`]];
   async execute() {
