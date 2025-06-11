80148f6f6be35064fc236eee8119bdebc383878a
Farzad Meow Khalafi <farzadk@gmail.com>
2025-06-11 12:52:18 -0400

    feat: silenting on test branch name: master

--- main-app/src/config/loggers.ts
+++ main-app/src/config/loggers.ts
@@ -2,6 +2,7 @@ import { ctxSafe } from "neko-helper/src";
 import { LogMessage } from "neko-logger/src";
 
 export default {
+  level: process.env.NODE_ENV === "test" ? "silent" : "info",
   extrasFunction: (message: LogMessage) => {
     let requestId = ctxSafe()?.get("requestId");
     requestId && (message.requestId = requestId);
