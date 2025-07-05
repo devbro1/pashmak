24319026509ccb7222b93ab3fc223d032d76ae22
meow meow <meow@devbro.com>
2025-07-05 16:20:11 +0000

    ci: fixes branch name: master

--- scripts/publish_to_npm.js
+++ scripts/publish_to_npm.js
@@ -7,29 +7,34 @@ const wcs = orderedWorkspaces.filter(
 );
 
 for (const wc of wcs) {
-  let remote_version = execSync(`npm view ${wc.name} version`, {
-    cwd: workspacePath,
-    stdio: "inherit",
-  });
+  let remote_version = "0.0.0";
+  try {
+    remote_version = execSync(`npm view ${wc} version`, {
+      cwd: workspacePath,
+    })
+      .toString()
+      .trim();
+  } catch {}
+
+  console.warn(wc, remote_version);
 
   let local_version = workspaces[wc].packageJson.version;
 
-  if (remote_version.toString().trim() === local_version) {
+  if (remote_version !== local_version) {
     console.log(
       `Skipping ${wc} as the local version matches the remote version.`,
     );
     continue;
   }
 
-  console.log(`Publishing ${wc}...`);
+  console.log(`Publishing ${wc} from ${remote_version} to ${local_version}...`);
   try {
-    execSync(`cd ${wc.location} && npm publish`, {
+    execSync(`cd ${workspaces[wc].location} && npm publish`, {
       cwd: workspacePath,
-      stdio: "inherit",
     });
-    console.log(`Successfully published ${wc.name}`);
+    console.log(`Successfully published ${wc}`);
   } catch (error) {
-    console.error(`Failed to publish ${wc.name}:`, error.message);
+    console.error(`Failed to publish ${wc}:`, error.message);
     process.exit(1);
   }
 }
