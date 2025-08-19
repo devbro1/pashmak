6789c77f95f43afd796b0216a585678067dcef22
meow meow <meow@devbro.com>
2025-08-19 02:10:54 +0000

    fix(ORM): added missing exports branch name: master

--- neko-orm/src/index.ts
+++ neko-orm/src/index.ts
@@ -1,3 +1,5 @@
 export * from './baseModel.mjs';
 export * from './decorators.mjs';
 export * from './relationships/index.mjs';
+export * from './LocalScopeQuery.mjs';
+export * from './GlobalScope.mjs';
