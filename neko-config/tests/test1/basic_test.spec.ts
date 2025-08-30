7bc6e143f62df85e50cc2d433e8b9a3a2117ab9f
Farzad Meow Khalafi <farzadk@gmail.com>
2025-08-30 02:06:26 -0400

    test: replace jest with vitest branch name: master

--- neko-config/tests/test1/basic_test.spec.ts
+++ neko-config/tests/test1/basic_test.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from 'vitest';
 
 describe('basic tests', () => {
   test('basic testing', async () => {});
--- neko-logger/tests/test1/basic_test.spec.ts
+++ neko-logger/tests/test1/basic_test.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from 'vitest';
 
 describe('general logger tests', () => {
   test('empty test', async () => {});
