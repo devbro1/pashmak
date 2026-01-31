bfa86ee1d6387a0909f701783e7b4d34bbed9acc
devbro1 <devbro1@users.noreply.github.com>
2026-01-31 03:44:56 +0000

    deploy: 752dabd049bc504735745d7b87c9018b722b249b

--- assets/js/acecf23e.a323f47b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdocs=globalThis.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/pashmak/blog","blogTitle":"Blog","authorsListPath":"/pashmak/blog/authors"}')}}]);
--- assets/js/f3955a3f.23714ab5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdocs=globalThis.webpackChunkdocs||[]).push([[2133],{768:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/pashmak/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
